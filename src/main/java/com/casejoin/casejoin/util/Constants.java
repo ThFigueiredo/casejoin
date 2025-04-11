1b5243af2dc5ff482c6d2b54396388006eb6d803
Thiago Figueiredo <thfigueiredo.developer@gmail.com>
2025-04-10 22:31:43 -0300

    feat: add custom exception handling for CategoriaNaoEncontradaException and global exception handler

---
+++ src/main/java/com/casejoin/casejoin/util/Constants.java
@@ -0,0 +1,11 @@
+package com.casejoin.casejoin.util;
+
+public class Constants {
+
+    public static final String CATEGORIA_NAO_ENCONTRADA = "Categoria não encontrada com ID: ";
+    public static final String PRODUTO_NAO_ENCONTRADO = "Produto não encontrado com ID: ";
+
+    private Constants() {}
+
+
+}
